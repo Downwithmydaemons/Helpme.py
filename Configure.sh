0d30a890124efab5de228a32a320ba8e3d2b2b72
Downwithmydaemons <DownwithmyDaemons@pm.me>
2024-06-29 18:20:46 -0400

    Update Configure.sh

--- Configure.sh
+++ Configure.sh
@@ -1,5 +1,5 @@
-!#/bin/bash
+#!/bin/bash
 
-sudo chmod 511 Enumhelp.py && sudo mv Enumhelp.py /usr/local/bin/Helpme
+sudo chmod 511 Enumhelp.py && sudo mv Enumhelp.py /usr/bin/Helpme
 
 echo 'Help me has been set up\n Use "Helpme" to call this tool'
